3e9ae434417a6409b21704edbf848854ef069059
adevr <alex.cst.reis@gmail.com>
2020-07-13 19:31:03 +0100

    feat: get request for aveiro

--- libraries/weather/index.js
+++ libraries/weather/index.js
@@ -11,6 +11,11 @@ const ipmaGlobal  ={
 
 const methods = {
     getForecast: () => {
+        let forecast = axios.get(ipmaGlobal.forecast_uri+ipmaGlobal.local_ids.aveiro+".json")
+            .then( response => {
+                console.log(response.data);
+        }   )
+
         // get forecast for which cities ?
         // maybe something should listen to the params and see if local exists
     }
