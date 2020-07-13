e05f5012823e932605feba92853e673e2f48a7ec
adevr <alex.cst.reis@gmail.com>
2020-07-13 19:53:50 +0100

    chore: added dynamic city

--- libraries/weather/index.js
+++ libraries/weather/index.js
@@ -4,21 +4,22 @@ let axios = require('axios')
 const ipmaGlobal  ={
     forecast_uri: "https://api.ipma.pt/open-data/forecast/meteorology/cities/daily/",
     local_ids : {
-        aveiro: 1010500
+        aveiro: 1010500,
+        coimbra: 1060300,
+        porto: 1131200,
+        lisboa: 1110600
     }
 }
 
 
 const methods = {
-    getForecast: () => {
-        let forecast = axios.get(ipmaGlobal.forecast_uri+ipmaGlobal.local_ids.aveiro+".json")
-            .then( response => {
-                console.log(response.data);
-        }   )
-
-        // get forecast for which cities ?
-        // maybe something should listen to the params and see if local exists
+    /**
+     * Get Forecast by city input
+     */
+    getForecast: async ( city ) => {
+        return await axios.get(ipmaGlobal.forecast_uri+ipmaGlobal.local_ids[city]+".json");
     }
+    
 }
 
 module.exports = methods
